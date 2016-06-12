65b81c0e2659fb09a399d1775c021fe61db13cb4
Dan McGuire <dan@metabahn.com>
2016-06-12 13:58:46 -0500

    path variable uneeded expand

--- lib/texas/client_writer.ex
+++ lib/texas/client_writer.ex
@@ -2,15 +2,17 @@ defmodule Texas.ClientAction do
   def define(prop) do
     quoted_client = client_send_back(prop)
     Code.eval_quoted(quoted_client)
-    quoted_channel_sub = channel_sub(prop)
-    Code.eval_quoted(quoted_channel_sub)
+    #quoted_channel_sub = channel_sub(prop)
+    #Code.eval_quoted(quoted_channel_sub)
   end
 
   defp client_send_back(prop) do
     quote do
-      path = "#{File.cwd!}/web/static/texas/"
-      File.mkdir_p!(unquote(path))
-      {:ok, file} = File.open(unquote(path) <> "#{unquote(prop)}.js", [:write])
+      path = "#{File.cwd!}/web/static/js/texas/"
+      file_path = path <> "#{unquote(prop)}.js"
+      File.mkdir_p!(path)
+      {:ok, file} = File.open(file_path, [:write])
+
       content =
         """
         $("data-prop='#{unquote(prop)}'").on("submit", function( event ) {
@@ -23,23 +25,23 @@ defmodule Texas.ClientAction do
     end
   end
 
-  defp channel_sub(prop) do
-    path = "#{File.cwd!}/web/static/texas/"
-    File.mkdir_p!(unquote(path))
-    quote do
-      {:ok, file} = File.open(unquote(path) <> "#{unquote(prop)}.js", [:write])
-      content =
-        """
-        var #{unquote(prop)}Html = document.querySelectorAll("[data-prop='#{unquote(prop)}']");
-        channel.on('#{unquote(prop)}', function(message){
-          var oldTree = convertHTML(#{unquote(prop)}Html.outerHTML);
-          var newHtml = message.message.split("\n").join("").trim();
-          var newTree = convertHTML(newHtml);
-          var chageset = diff(oldTree, newTree);
-          #{unquote(prop)}Html = patch(#{unquote(prop)}Html, changeset);
-        """
-      IO.binwrite file, content
-      File.close file
-    end
-  end
+  #defp channel_sub(prop) do
+    #path = "#{File.cwd!}/web/static/texas/"
+    #File.mkdir_p!(unquote(path))
+    #quote do
+      #{:ok, file} = File.open(unquote(path) <> "#{unquote(prop)}.js", [:write])
+      #content =
+        #"""
+        #var #{unquote(prop)}Html = document.querySelectorAll("[data-prop='#{unquote(prop)}']");
+        #channel.on('#{unquote(prop)}', function(message){
+          #var oldTree = convertHTML(#{unquote(prop)}Html.outerHTML);
+          #var newHtml = message.message.split("\n").join("").trim();
+          #var newTree = convertHTML(newHtml);
+          #var chageset = diff(oldTree, newTree);
+          ##{unquote(prop)}Html = patch(#{unquote(prop)}Html, changeset);
+        #"""
+      #IO.binwrite file, content
+      #File.close file
+    #end
+  #end
 end
