b7fe9ea25411aadbcfba248d323308c94fbbe3c2
Dan McGuire <dan@metabahn.com>
2016-06-12 13:32:58 -0500

    writes some transform client js and hard couples to phoenix for now

--- lib/texas/client_writer.ex
+++ lib/texas/client_writer.ex
@@ -1,18 +1,45 @@
 defmodule Texas.ClientAction do
-  def define(scope, path \\ File.cwd!) do
-    quoted = quote do
-      {:ok, file} = File.open("#{unquote(path)}/#{unquote(scope)}.js", [:write])
+  def define(prop) do
+    quoted_client = client_send_back(prop)
+    Code.eval_quoted(quoted_client)
+    quoted_channel_sub = channel_sub(prop)
+    Code.eval_quoted(quoted_channel_sub)
+  end
+
+  defp client_send_back(prop) do
+    quote do
+      path = "#{File.cwd!}/web/static/texas/"
+      File.mkdir_p!(path)
+      {:ok, file} = File.open(path <> "#{unquote(prop)}.js", [:write])
       content =
         """
-        $("data-scope='#{unquote(scope)}'").on("submit", function( event ) {
+        $("data-prop='#{unquote(prop)}'").on("submit", function( event ) {
           event.preventDefault();
-          channel.push("scope:#{unquote(scope)}", {query: $(event.target).serialize()});
+          channel.push("prop:#{unquote(prop)}", {query: $(event.target).serialize()});
         });
         """
       IO.binwrite file, content
       File.close file
     end
-    Code.eval_quoted(quoted)
-    path
+  end
+
+  defp channel_sub(prop) do
+    path = "#{File.cwd!}/web/static/texas/"
+    File.mkdir_p!(path)
+    quote do
+      {:ok, file} = File.open(path <> "#{unquote(prop)}.js", [:write])
+      content =
+        """
+        var #{unquote(prop)}Html = document.querySelectorAll("[data-prop='#{unquote(prop)}']");
+        channel.on('#{unquote(prop)}', function(message){
+          var oldTree = convertHTML(#{unquote(prop)}Html.outerHTML);
+          var newHtml = message.message.split("\n").join("").trim();
+          var newTree = convertHTML(newHtml);
+          var chageset = diff(oldTree, newTree);
+          #{unquote(prop)}Html = patch(#{unquote(prop)}Html, changeset);
+        """
+      IO.binwrite file, content
+      File.close file
+    end
   end
 end
