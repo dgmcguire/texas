4d980bc067e0e48fcfde2cd9a7fe7e7225201236
Dan McGuire <dan@metabahn.com>
2016-11-02 16:13:59 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Texas.Mixfile do
 
   def project do
     [app: :texas,
-     version: "0.1.2",
+     version: "0.1.3",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
